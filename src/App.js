11a72124f13961d510e6244d662effd707559487
KasunW <kasun.w@duosoftware.com>
2019-03-30 16:17:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends Component {
             UserPoolId: awsweb.Auth.userPoolId,
             AppWebDomain: awsweb.Auth.appWebDomain,
             TokenScopesArray: ['email', 'openid'],
-            RedirectUriSignIn: 'http://dev.smoothflow.io',
-            RedirectUriSignOut: 'http://dev.smoothflow.io',
+            RedirectUriSignIn: 'http://dev.smoothflow.io/account',
+            RedirectUriSignOut: 'http://dev.smoothflow.io/account',
             ResponseType: 'code',
             IdentityProvider : 'Google'
         };
